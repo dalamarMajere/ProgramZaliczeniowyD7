2a490e77bb777da64a76801427015122b3afb437
KamilH7 <kamilharaf7@gmail.com>
2020-01-08 16:13:51 +0100

    Changed resolution

--- src/application/Main.java
+++ src/application/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 	 public void start(Stage primaryStage) throws Exception{
 	        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 	        primaryStage.setTitle("USOSWEB Editor");
-	        primaryStage.setScene(new Scene(root, 1000, 800));
+	        primaryStage.setScene(new Scene(root, 1000, 850));
 	        primaryStage.show();
 	 }
 
