a2523578597b743f0a9dce39390ab6fd75cf2af1
KamilH7 <kamilharaf7@gmail.com>
2020-01-08 15:41:25 +0100

    Updated menu

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,19 +1,11 @@
 package application;
 
-
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import java.awt.TextArea;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
-
-import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 
 public class Controller implements Initializable {
@@ -24,7 +16,7 @@ public class Controller implements Initializable {
      * @see javafx.fxml.Initializable#initialize(java.net.URL, java.util.ResourceBundle)
      */
 
-    public void initialize(URL location, ResourceBundle resources) {
+     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("View is now loaded!");
     }
 
@@ -34,73 +26,40 @@ public class Controller implements Initializable {
 	    /*
 	     * STUDENT
 	     */
+
 	    public TextField studentCryteriumLabel;
-		public ChoiceBox<String> studentChoicebox;
-		public ChoiceBox<String> studentSubjectChoicebox;
+	    public TextArea studentTextArea;
 	    public TextField studentIdLabel;
 	    public TextField studentSurnameLabel;
 	    public TextField studentNameLabel;
 	    public TextField studentSecondNameLabel;
 	    public TextField studentBirthDateLabel;
-	    public ChoiceBox<String> studentFacultyChoiceBox;
+	    public ChoiceBox<?> studentFacultyChoiceBox;
+	    public ChoiceBox<?> studentCourseChoiceBox;
 	    public TextField studentSemesterLabel;
-	    public TextField studentGradesLabel;
 
 		    public void studentAddButtonAction() {
 
-		    	String container = studentCryteriumLabel.getText();
-		    	System.out.println("Searching for.. "+container);
-		    	ObservableList<String> list = FXCollections.observableArrayList(container);
-		    	studentChoicebox.setItems(list);
-		    	studentChoicebox.setValue(list.get(0));
-
 		    }
+
 
 		    public void studentSearchButtonAction() {
 
 		    }
 
-		    public void studentInfoAbortButtonAction() {
-
-		    }
-
-
-		    public void studentInfoSaveButtonAction() {
-
-		    }
-
-
-		    public void studentGradeAbortButtonAction() {
-
-		    }
-
-		    public void studentGradeSaveButtonAction() {
-
-		    }
-
 	    /*
-		 * TEACHER
-		 */
-		public TextField employeeCryteriumLabel;
-	    public ChoiceBox<?> employeeChoicebox;
-	    public ChoiceBox<?> employeeSubjectChoicebox;
+	     * EMPLOYEE
+	     */
+
+	    public TextField employeeCryteriumLabel;
+	    public TextArea employeeTextArea;
 	    public TextField employeeIdLabel;
 	    public TextField employeeTitleLabel;
 	    public TextField employeeSurnameLabel;
 	    public TextField employeeNameLabel;
 	    public TextField employeeBirthDate;
 
-			public void employeeAbortButtonAction() {
-
-		    }
-
-
 		    public void employeeAddButtonAction() {
-
-		    }
-
-
-		    public void employeeSaveButtonAction() {
 
 		    }
 
@@ -109,37 +68,22 @@ public class Controller implements Initializable {
 
 		    }
 
-
-		    public void employeeSubjectAbortButtonAction() {
-
-		    }
-
-
-		    public void employeeSubjectSaveButtonAction() {
-
-		    }
-
 	    /*
 	     * SUBJECT
 	     */
+
 	    public TextField subjectCryteriumLabel;
-	    public ChoiceBox<?> subjectChoicebox;
+	    public TextArea subjectTextArea;
 	    public TextField subjectNameLabel;
 	    public ChoiceBox<?> subjectUnitChoiceBox;
 
-		    public void subjectSearchButtonAction() {
-
-		    }
 
 		    public void subjectAddButtonAction() {
 
 		    }
 
-		    public void subjectAbortButtonAction() {
 
-		    }
-
-		    public void subjectSaveButtonAction() {
+		    public void subjectSearchButtonAction() {
 
 		    }
 
@@ -148,24 +92,16 @@ public class Controller implements Initializable {
 	     */
 
 	    public TextField courseCriteriumLabel;
-	    public ChoiceBox<?> courseChoicebox;
-	    public ChoiceBox<?> courseSubjectChoicebox;
+	    public TextArea courseTextArea;
 	    public TextField courseIdLabel;
 	    public TextField courseNameLabel;
-
-		    public void courseSearchButtonAction() {
-
-		    }
 
 		    public void courseAddButtonAction() {
 
 		    }
 
-		    public void courseAbortButtonAction() {
 
-		    }
-
-		    public void courseSaveButtonAction() {
+		    public void courseSearchButtonAction() {
 
 		    }
 
@@ -174,29 +110,19 @@ public class Controller implements Initializable {
 	     */
 
 	    public TextField facultyCriteriumLabel;
-	    public ChoiceBox<?> facultyChoicebox;
-	    public ChoiceBox<?> facultyCourseChoicebox;
+	    public TextArea facultyTextArea;
 	    public TextField facultyIdLabel;
 	    public TextField facultyNameLabel;
 
-	    	public void facultySearchButtonAction() {
 
-	    	}
 
 		    public void facultyAddButtonAction() {
 
 		    }
 
-		    public void facultyAbortButtonAction() {
+
+		    public void facultySearchButtonAction() {
 
 		    }
-
-		    public void facultySaveButtonAction() {
-
-		    }
-
-
-
-
 
 }
